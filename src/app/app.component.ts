0450901296acf78832b9ed546adb9a007c9024ec
hadinasiri.6970 <hadinasiri.6970@gmail.com>
2022-06-28 22:45:31 +0430

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormatDate, FormatNumber, FORMAT_NUMBER } from 'pl-decorator';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formatDate';
+  @FormatNumber([FORMAT_NUMBER.AR])
+  public value: number;
+
+  /**declare variable currency with FormatNumber annotation, for format it  */
+  @FormatNumber(FORMAT_NUMBER.HI, { style: 'currency', currency: 'EUR' })
+  public currency: number;
+
+  /**declare variable value with FormatDate annotation, for format it */
+  @FormatDate("DD/MM/YYYY HH:mm:ss")
+  public time: Date ;
+
+  /**valorize time variable with current or other Date*/
+  constructor() {
+    this.time = new Date();
+    this.value = 2736288199283779;
+    this.currency=2144287;
+  }
+
+  /** print variable value */
+  ngOnInit() {
+    console.log(`                 Date : ${new Date()} , 
+                 Date formatted : ${this.time} 
+                 Value formatted:  ${this.value}  
+                 Currency formatted:  ${this.currency}`);
+  }
 }
